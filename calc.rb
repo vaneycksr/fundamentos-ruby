488dff79605bae74208eb092350c78fc70a90574
vaneycksr <vaneyck23@gmail.com>
2020-03-31 00:26:18 -0300

    conceitos basico de ruby

---
+++ calc.rb
@@ -0,0 +1,12 @@
+#print "Digite o primeiro número inteiro: "
+
+# .to_i transforma a string em um inteiro
+A = gets.chomp.to_i
+
+#print "Digite o segundo número inteiro: "
+
+B = gets.chomp.to_i
+
+X = A + B
+
+puts "X = #{X}\n"
---
+++ conceitos.rb
@@ -0,0 +1,12 @@
+# o cursos fica na mesma linha
+print "Digite seu nome: "
+
+# entrada do teclado
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+
+sobrenome = gets.chomp
+
+# concatena dentro da string usando #{variavel}
+puts "ola #{nome} #{sobrenome}!"
