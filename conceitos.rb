8c19d28a476e1f45d4e5a65c68a46b23417ecd96
vaneycksr <vaneyck23@gmail.com>
2020-04-14 22:21:21 -0300

    criando metodos

--- conceitos.rb
+++ conceitos.rb
@@ -1,16 +1,24 @@
+# frozen_string_literal: true
+
 # o cursos fica na mesma linha
-print "Digite seu nome: "
+# print 'Digite seu nome: '
 
 # entrada do teclado
-num1 = gets.chomp.to_i
+# num1 = gets.chomp.to_i
 
-print "Digite sua idade: "
+# print 'Digite sua idade: '
 
-num2 = gets.chomp.to_i
+# num2 = gets.chomp.to_i
 
 # concatena dentro da string usando #{variavel}
-#puts "ola meu nome é #{nome} e minha idade é #{idade}!"
-soma = num1 + num2
-sub = num1 - num2
-div = num1 / num2
-puts "soma: #{soma}\nsubtração: #{sub}\ndivisão: #{div}"
+# puts "ola meu nome eh #{nome} e minha idade eh #{idade}!"
+# soma = num1 + num2
+# sub = num1 - num2
+# div = num1 / num2
+# puts "soma: #{soma}\nsubtracao: #{sub}\ndivisao: #{div}"
+
+v1 = 10
+v2 = 11
+
+# igual a eql? objetos sao iguais
+puts v1.eql?(v2)
---
+++ metodos.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+def diga_ola(nome)
+  puts 'Olá! ' + nome
+end
+
+diga_ola('Van')
+
+# metodo com retorno
+# retorna a ultima instrucao de codigo
+def retorna_nome
+  'Van'
+end
+
+# resultado = retorna_nome
+# puts resultado
+
+# funcao com parametro
+def soma(v1, v2)
+  total = v1 + v2
+  total
+end
+
+# res = soma(10, 25)
+# puts res
