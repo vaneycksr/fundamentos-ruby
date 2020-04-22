58826993bf1828464865a52e604229554924660a
vaneycksr <vaneyck23@gmail.com>
2020-04-21 22:36:15 -0300

    alguns validadores e macthers do rspec

--- unitarios/spec/avengers_spec.rb
+++ unitarios/spec/avengers_spec.rb
@@ -25,6 +25,51 @@ describe AvengersHeadQuarter do
     # invoca o metodo
     hq.put('Spiderman')
 
-    expect(hq.list).to eql ['Spiderman']
+    expect(hq.list).to include 'Spiderman'
+  end
+
+  it 'deve adicionar uma lista de vingadores' do
+    hq = AvengersHeadQuarter.new
+
+    hq.put('Thor')
+    hq.put('Hulk')
+    hq.put('Spiderman')
+
+    # verifica se na lista existe Thor
+    # expect(hq.list).to include 'Thor'
+
+    # verifica se o tamanho da lista eh maior que zero
+    expect(hq.list.size).to be > 0
+  end
+
+  it 'thor deve ser o primeiro da lista' do
+    hq = AvengersHeadQuarter.new
+
+    hq.put('Thor')
+    hq.put('Hulk')
+    hq.put('Spiderman')
+
+    expect(hq.list).to start_with('Thor')
+  end
+
+  it 'ironman deve ser o último da lista' do
+    hq = AvengersHeadQuarter.new
+
+    hq.put('Thor')
+    hq.put('Hulk')
+    hq.put('Spiderman')
+    hq.put('Ironman')
+
+    expect(hq.list).to end_with('Ironman')
+  end
+
+  it 'deve conter o sobrenome' do
+    avenger = 'Peter Parker'
+
+    # verifica com expressao regular se contem uma palavra na string
+    expect(avenger).to match(/Par/)
+
+    # verifica com expressao regular se nao contem uma palavra na string
+    expect(avenger).not_to match(/van/)
   end
 end
